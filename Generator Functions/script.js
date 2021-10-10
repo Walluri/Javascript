8a779e4f026b130489114f3f9e94b7050177098c
Valluri Satyaprakash <prakash4455@gmail.com>
2021-10-10 22:33:06 +0530

    Generator function : Theory explained

--- Generator Functions/script.js
+++ Generator Functions/script.js
@@ -5,12 +5,24 @@ function* generatorFoo(i) {
   yield i + 20;
 }
 
-//get the generator object
+//call the generator function to get an iterator object
+// An iterator object contains 3 methods {next:f(),return:f(),throw:f()}
 const gen = generatorFoo(1);
+console.log(gen);
 document
   .querySelector("body")
   .appendChild(document.createTextNode(gen.next().value));
 
+// When the iterator objects next method is called then the generator
+// .. functions body is executed until the  first yeild expression.
+
+// The yeild specifies the value to be returned from the iterator
+//Or
+// yeild* delegates to another generator function.
+
+//Now the next method returns an object with a value property containing the yeilded value.
+// +
+// a done property which indicates if the generator has yeilded its last value
 document
   .querySelector("body")
   .appendChild(document.createTextNode(gen.next().value));
