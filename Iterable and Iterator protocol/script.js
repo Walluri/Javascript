f865684fa1001914cc3333fe4b4c0254f3820b48
Valluri Satyaprakash <prakash4455@gmail.com>
2021-10-12 23:14:27 +0530

    Iterator and Iterable protocol : with 3 examples

--- Iterable and Iterator protocol/script.js
+++ Iterable and Iterator protocol/script.js
@@ -4,20 +4,64 @@
 
 //i.e @@iterator === [symbol.iterator]
 
-//The property [Symbol.iterator] is a zero argument function that returns an object..
-//...conforming to the ITERATOR protocol.
-
+//Case 1 : This can be a generator function or a ordinary function
 let obj = {
-  //This can be a ordinary function or a generator function
-  // [Symbol.iterator]: function* () {
-  //   yield 10;
-  //   yield 20;
-  // },
-  // this function when invoked should return a ITERATOR  Object - which will be used to obtain the values to be iterated.
+  [Symbol.iterator]: function* () {
+    yield 10;
+    yield 20;
+  },
 };
 
 for (const iterator of obj) {
   console.log(iterator);
 }
 
-//Now lets take a look at the ITERATOR PROTOCOL
+//Case 2 :
+//The property [Symbol.iterator] is a zero argument function that returns an object..
+//...conforming to the ITERATOR protocol.
+
+// ITERATOR PROTOCOL defines a standard way to produce a sequence of values.
+// This function when invoked should return a ITERATOR  Object - which will be used to obtain the values to be iterated.
+// An object is an iterator when it implements the next() method which returns {value:10,done:false}
+
+let obj2 = {
+  [Symbol.iterator]: function () {
+    return {
+      InteArray: ["apple", "banana"],
+      pointer: -1,
+      next: function () {
+        this.pointer++;
+        if (this.pointer === this.InteArray.length) return { done: true };
+        return { done: false, value: this.InteArray[this.pointer] };
+      },
+    };
+  },
+};
+console.log("--------------------");
+for (const iterator of obj2) {
+  console.log(iterator);
+}
+
+//case 3 :
+//Why do you have to always have local variables to the object returned.
+let obj3 = {
+  names: ["mike", "smith", "jonas"],
+  pointer: -1,
+  //This time we are not going to have a generator function but a normal function
+  [Symbol.iterator]: function () {
+    //return a object that conforms to the iterator protocol i.e an object that has a next method.
+    // it is simple implement the next method in the current object and just return this
+    return this;
+  },
+  //how should the iteration behave
+  next: function () {
+    this.pointer++;
+    if (this.pointer === this.names.length) return { done: true };
+    return { done: false, value: this.names[this.pointer] };
+  },
+};
+
+console.log("----------------------");
+for (const iterator of obj3) {
+  console.log(iterator);
+}
