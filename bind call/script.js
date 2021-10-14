f62237253351e80f9e4ffd4ecce5f1a132f5ad08
Valluri Satyaprakash <prakash4455@gmail.com>
2021-10-15 01:47:19 +0530

    Other use cases of using bind call of function

--- bind call/script.js
+++ bind call/script.js
@@ -22,3 +22,37 @@ console.log(boundedGetX()); // prints 20 as it is bound properly
 // is to make a function, no matter how it is called is called with a particular this value.
 
 //Example 2 : Another use of bind is to make a function with pre specified initial arguments
+function List() {
+  return Array.prototype.slice.call(arguments);
+}
+
+function addArgument(arg1, arg2) {
+  return arg1 + arg2;
+}
+
+const list1 = List(1, 2, 3);
+
+const result1 = addArgument(1, 2);
+
+const LeadingthirtysevenList = List.bind(null, 37);
+
+const otherLeadingthirtysevenList = LeadingthirtysevenList(2, 3, 4);
+console.log(otherLeadingthirtysevenList);
+
+//Example 3 : bind with setTimeOut
+// the scope inside settimeout is window scope... so one should bind the 'this' properly before passing a callback to settimeout
+
+function LateBloomer() {
+  this.petalCount = Math.ceil(Math.random() * 12);
+}
+
+LateBloomer.prototype.declare = function () {
+  console.log(`No of Petals : ${this.petalCount}`);
+};
+
+LateBloomer.prototype.bloom = function () {
+  setTimeout(this.declare.bind(this), 1000);
+};
+
+let flowerObj = new LateBloomer();
+flowerObj.bloom();
